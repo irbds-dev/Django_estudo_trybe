ff969edfde682d6d9a9af0a12fd341acdfb4bc27
Igor Briano <igor.briano1998@gmail.com>
2026-03-22 20:00:26 -0300

    Criado documento Readme com o passo a passo para se criar um novo projeto

---
+++ schedule/database/01_create_database.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS `schedule_database`;
+USE `schedule_database`;
--- schedule/schedule/settings.py
+++ schedule/schedule/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "tasks",
 ]
 
 MIDDLEWARE = [
@@ -73,9 +74,13 @@ WSGI_APPLICATION = 'schedule.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "schedule_database",
+        "USER": "root",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
